ceea2ea06025176594cbbb9afaa99ce7562680c0
Paul Cadman <git@paulcadman.io>
2018-03-06 10:20:13 +0000

    Initial Commit

---
+++ PoorPeformance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PoorPeformance
+//
+//  Created by Paul Cadman on 06/03/2018.
+//  Copyright © 2018 Paul Cadman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
